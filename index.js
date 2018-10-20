e3d71f6df2f5d21eb8939c5b6db47d70b5a5bfe4
Will Hill <email.will.in.china@gmail.com>
2018-10-20 22:44:15 +0800

    options

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const HDMW = require('oip-hdmw')
 const Wallet = HDMW.Wallet;
 
-// let myWallet = new Wallet();
-//const mnemonic = myWallet.getMnemonic()
-
-const myWallet = new Wallet('carbon panda replace drum guess heart inside useless random bulb hint industry');
+const options = {
+    discover: false,
+    supported_coins: ['flo'],
+    networks: [],
+}
+const myWallet = new Wallet('carbon panda replace drum guess heart inside useless random bulb hint industry', options);
 const coins = myWallet.getCoins()
 
-console.log(`My Wallets Coins: ${coins}`)
-//console.log(`My Mnemonic:${mnemonic}`)
+console.log('My Wallets Coins:')
+console.log(coins)
+
